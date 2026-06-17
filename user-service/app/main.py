e9f64f8ebfeb7284be35bb641bb235c5083a62be
David-19960111 <david11011966@gmail.com>
2026-06-17 01:18:39 -0300

    "feat: add PostgreSQL to user-service"

--- user-service/app/main.py
+++ user-service/app/main.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
 from app.routes import router
+from app.database import engine
+from app import models
 from prometheus_fastapi_instrumentator import Instrumentator
+import logging
 import os
 
 load_dotenv()
 
-app = FastAPI(title="User Service", version="1.0.0")
+models.Base.metadata.create_all(bind=engine)
+
+logging.basicConfig(level=logging.INFO)
+
+app = FastAPI(title="User Service", version="1.0.0",
+    docs_url="/docs" if os.getenv("ENV") != "production" else None, redoc_url=None)
 
 app.include_router(router, prefix="/users")
 
--- user-service/app/routes.py
+++ user-service/app/routes.py
@@ -1,32 +1,36 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
+from sqlalchemy.orm import Session
 from app.schemas import UserRegister, UserLogin, TokenResponse
 from app.auth import hash_password, verify_password, create_token
+from app.models import User
+from app.database import get_db
 
 router = APIRouter()
 
-#Base de datos en memoria por ahora
-users_db = {}
-
 @router.post("/register")
-def register(user: UserRegister):
-    if user.email in users_db:
-        raise HTTPException(status_code=400, detail="Email ya registrado.")
+def register(user: UserRegister, db: Session = Depends(get_db)):
+    existing = db.query(User).filter(User.email == user.email).first()
+    if existing:
+        raise HTTPException(status_code=400, detail="Email ya registrado")
     
-    users_db[user.email] = {
-        "email": user.email,
-        "full_name": user.full_name,
-        "password": hash_password(user.password)
-    }
-
-    return {"mensaje": "Usuario registrado correctamente"}
+    new_user = User(
+        email=user.email,
+        full_name=user.full_name,
+        hashed_password=hash_password(user.password)
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    
+    return {"mensaje": "Usuario registrado correctamente", "id": str(new_user.id)}
 
 @router.post("/login", response_model=TokenResponse)
-def login(user: UserLogin):
-    db_user = users_db.get(user.email)
+def login(user: UserLogin, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.email == user.email).first()
     
-    if not db_user or not verify_password(user.password, db_user["password"]):
+    if not db_user or not verify_password(user.password, db_user.hashed_password):
         raise HTTPException(status_code=401, detail="Credenciales incorrectas")
     
-    token = create_token({"sub": user.email, "name": db_user["full_name"]})
+    token = create_token({"sub": db_user.email, "name": db_user.full_name})
     
     return {"access_token": token, "token_type": "bearer"}
